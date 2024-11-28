939aee9529e969f4372a1ad722f5acd04abe4744
Aayan Ahmed <aayanahmedtejani@gmail.com>
2024-11-29 00:22:12 +0500

    ?1 useRef done

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,13 @@
+// import { CounterRef , StateMemo , TimerComponent , FocusInput , EffectRef , MultiRef , ConditionalRef , RefVsState} from './hooks/03-UseRef/01-Guess-Output'
+// import MouseTracker from './components/03-HOCs'
 import React from 'react'
 import UseEffect from './hooks/02-UseEffect'
-import { CounterRef , StateMemo , TimerComponent , FocusInput , EffectRef , MultiRef , ConditionalRef , RefVsState} from './hooks/03-UseRef/01-Guess-Output'
-import MouseTracker from './components/03-HOCs'
+import {FocusInput} from './hooks/03-UseRef/02-Basic-Ques'
 
 
 const App = () => {
   return (
-    <MouseTracker 
-      render={({x, y}) => (
-        <p>Mouse position: {x}, {y}</p>
-      )}
-    />
+    <FocusInput />
   )
 }
 
--- src/hooks/03-UseRef/02-Basic-Ques.jsx
+++ src/hooks/03-UseRef/02-Basic-Ques.jsx
@@ -1,5 +1,25 @@
-//? Create a component with a button that focuses on an input field when clicked:
-//? Create a video player component where you can play/pause video using useRef:
-//? Create a component that counts how many times it rendered without causing re-renders:
-//? Create a color picker where clicking a button copies the color code to clipboard using useRef:
-//? Create a scroll-to-top button that appears after scrolling down:
+//?1 Create a component with a button that focuses on an input field when clicked:
+//?2 Create a video player component where you can play/pause video using useRef:
+//?3 Create a component that counts how many times it rendered without causing re-renders:
+//?4 Create a color picker where clicking a button copies the color code to clipboard using useRef:
+//?5 Create a scroll-to-top button that appears after scrolling down:
+
+//*1
+import React, { useRef } from 'react'
+
+const FocusInput = () => {
+  const inputRef = useRef(null)
+
+  function handleFocus (){
+    input.current.focus()
+  }
+
+  return (
+    <div>
+      <input type="text" ref={inputRef}/>
+      <button onClick={handleFocus}>Focus</button>
+    </div>
+  )
+}
+
+export {FocusInput}
